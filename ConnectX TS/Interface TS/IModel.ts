e391a50a02377cdad71d18df987a22719d7c616a
Arnesh997 <90772757+Arnesh997@users.noreply.github.com>
2023-02-09 12:23:14 +0000

    Imodel & Connect Four class created

---
+++ ConnectX TS/Interface TS/IModel.ts
@@ -0,0 +1,11 @@
+interface Imoddel{
+    CONCEDE_MOVE: number,
+    GAME_STATUS_WIN_1: number,
+    GAME_STATUS_WIN_2: number,
+    GAME_STATUS_TIE: number
+
+    MIN_ROWS: number,
+    MAX_ROWS: number,
+    MIN_COLS: number,
+    MAX_COLS: number
+}
